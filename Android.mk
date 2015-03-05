1362a41fa52c4a4bf1c4b41480b6f7cf5b027a63
Mr-zeng <zengwenhao0710@gmail.com>
2015-03-05 23:44:40 +0800

    MTK Backup Code

---
+++ Android.mk
@@ -0,0 +1,19 @@
+ifneq ($(TARGET_SIMULATOR),true)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+ifeq ($(BOARD_RECOVERY_MTK),true)
+LOCAL_SRC_FILES := \
+	mmcutils_mtk.c
+else
+LOCAL_SRC_FILES := \
+	mmcutils.c
+endif
+
+LOCAL_MODULE := libmmcutils
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_STATIC_LIBRARY)
+
+endif	# !TARGET_SIMULATOR
